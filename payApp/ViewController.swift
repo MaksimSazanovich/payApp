8337911823d56d942b832b2ab7585bca7eddaddc
Unity_one_love <sazanovichmaksim@gmail.com>
2026-01-12 20:33:12 +0300

    Initial Commit

---
+++ payApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  payApp
+//
+//  Created by Maksim on 12.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
